ee71b6907cebc54a5179f801d54e7b386ef75247
snwzd <145876397+snwzd@users.noreply.github.com>
2024-11-26 01:11:31 +0530

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://snwzd.github.io',
-  base: 'website',
+  // base: 'website',
   integrations: [tailwind()]
 });
